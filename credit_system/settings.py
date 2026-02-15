cff8c061c9078c9edfcc15213afcd40a99851b98
Pratish Sharma <pratishsharma19@gmail.com>
2026-02-15 19:00:30 +0530

    Make app cloud-ready

--- credit_system/settings.py
+++ credit_system/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-^_h_3$cqit^%rdojr8mpdw)y5=4%ix69fqiq+08#e2mq#43w7o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -73,16 +75,12 @@ WSGI_APPLICATION = 'credit_system.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+# Docker uses the 'default' string. The Cloud will provide a DATABASE_URL.
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
-    }
+    'default': dj_database_url.config(
+        default='postgres://postgres:postgres@db:5432/postgres',
+        conn_max_age=600
+    )
 }
 
 
@@ -128,6 +126,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Celery Configuration
-CELERY_BROKER_URL = 'redis://redis:6379/0'
+# Use REDIS_URL from cloud, or fallback to local docker redis
+CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://redis:6379/0')
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
